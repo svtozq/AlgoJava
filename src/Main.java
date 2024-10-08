4f3c85ba342be9310ada1588e2470140eda7cdee
Robin PENEA <rpenea@esiee-it>
2024-10-08 14:15:09 +0200

    TP#03

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
 
 public class Main {
+
+
+    /**
+     * Function called "Tableau" taking in parameter Tab (Tab an int type parameter)
+     * "Tableau" go throughout Tab
+     * Show each Tab value
+     */
     //On déclare une fonction Tableau qui prend en paramètre Tab (tableau des scores)
     public static void Tableau (int[] Tab){
-
         //Parcourir Tab
         for (int i = 0; i < Tab.length; i++) {
+            //Afficher chaque case
             System.out.println(Tab[i]);
         }
     }
 
+
+    /**
+     * Function called "MaxTab" taking in parameter Tab (Tab an int type parameter)
+     * "MaxTab" go throughout Tab
+     * Default int max = first case
+     * If case value is below to first case then max takes this value and if the next value is still below then max takes this new value
+     * Show Tab's max value
+     */
     //On déclare une fonction MaxTab qui prend en paramètre Tab (tableau des scores)
     public static void MaxTab (int[] Tab){
         int max = Tab[0];
@@ -18,11 +33,18 @@ public class Main {
             //Si la valeur de l'index est > à "Tab[0]" (int max) alors int max prend la nouvelle valeur de l'index et l'affiche
             if ( Tab[i] > max ) {
                 max = Tab[i];
+                //Afficher max
                 System.out.println("maximum = " +max);
             }
         }
     }
 
+
+    /**
+     * Function called "Vrai" taking in parameter Tab (Tab an int type parameter)
+     * "Vrai" go throughout Tab
+     * If case value is under 10 then teh function returns true else retuens false
+     */
     //On déclare une fonction Vrai qui prend en paramètre Tab (tableau des scores)
     public static boolean Vrai (int[] Tab){
         //Parcourir Tab
@@ -34,6 +56,7 @@ public class Main {
         }
         return false;
     }
+
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
