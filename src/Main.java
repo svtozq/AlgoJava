8d57ebbc4c19914d8fc031bce090e8ee84dd023b
Robin PENEA <rpenea@esiee-it>
2024-10-08 11:43:46 +0200

    TP#02

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 
 public class Main {
+    //On déclare une fonction Tableau qui prend en paramètre Tab (tableau des scores)
+    public static void Tableau (int[] Tab){
+
+        //Parcourir Tab
+        for (int i = 0; i < Tab.length; i++) {
+            System.out.println(Tab[i]);
+        }
+    }
+
+    //On déclare une fonction MaxTab qui prend en paramètre Tab (tableau des scores)
+    public static void MaxTab (int[] Tab){
+        int max = Tab[0];
+
+        //Parcourir Tab
+        for (int i = 0; i < Tab.length; i++) {
+            //Si la valeur de l'index est > à "Tab[0]" (int max) alors int max prend la nouvelle valeur de l'index et l'affiche
+            if ( Tab[i] > max ) {
+                max = Tab[i];
+                System.out.println("maximum = " +max);
+            }
+        }
+    }
+
+    //On déclare une fonction Vrai qui prend en paramètre Tab (tableau des scores)
+    public static boolean Vrai (int[] Tab){
+        //Parcourir Tab
+        for (int i = 0; i < Tab.length; i++) {
+            //Si la valeur de l'index est < 10 alors la fonction renvoie vrai
+            if ( Tab [i] < 10 ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
@@ -17,34 +52,10 @@ public class Main {
         TabScore[5] = 50;
         TabScore[6] = 34;
 
-//        Valeurs random à TabScore
-//        On attribue des valeurs aléatoires à chaque case de TabScore
-//        Random random = new Random();
-//        for (int i = 0; i < TabScore.length; i++) {
-//            On tire un nombre aléatoire jusqu'à 101 exclu et on assigne le nombre à la variable number
-//            int number = random.nextInt(101);
-//            TabScore[i] = number;
-//        }
-
-        //Parcourir TabScore et afficher chaque case
-        //for ( DEPART; CONDITION D'ARRÊT; INCRÉMENT)
-        //"for" = boucle
-        for (int i = 0; i < TabScore.length; i++) {
-            //Afficher chaque case
-            int score = TabScore[i];
-            System.out.println(score);
-        }
-
-        //Parcourir TabScore et ajout de chaque valeur dans moyenne
-        float moyenne = 0;
-
-        for (int i = 0; i < TabScore.length; i++){
-            moyenne = moyenne + TabScore[i];
-        }
-
-        //Afficher la moyenne
-        moyenne = moyenne / TabScore.length;
-        System.out.println("moyenne = " +moyenne);
+        Tableau(TabScore);
+        MaxTab(TabScore);
+        System.out.println(Vrai(TabScore));
 
     }
+
 }
